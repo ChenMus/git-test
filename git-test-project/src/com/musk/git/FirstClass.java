7ba30a23278d0382cddf745e097faadd43e559b4
Cmushi <chenms0083@163.com>
2020-04-14 23:14:21 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/musk/git/FirstClass.java
@@ -0,0 +1,11 @@
+package com.musk.git;
+
+/**
+ * @Author Cmushi
+ * @create 2020/4/14 22:59
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project!");
+    }
+}
